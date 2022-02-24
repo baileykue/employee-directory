b6edcddd306949fc92faa32b9c9e30ee8ee3a98d
Bailey Kuebelbeck <bailey.kue@gmail.com>
2022-02-24 11:39:17 -0800

    adding context and starting to write services functions

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,1 +1,28 @@
-import { createContext } from 'react';
+import { createContext, useContext, useState } from 'react';
+
+export const UserContext = createContext();
+
+const UserProvider = ({ children }) => {
+  const currentUser = getUser();
+  const [user, setUser] = useState(
+    currentUser ? { id: currentUser.id, email: currentUser.email } : {}
+  );
+
+  const userValues = { user, setUser };
+
+  return (
+    <UserContext.Provider value={userValues}>{children}</UserContext.Provider>
+  );
+};
+
+const useUser = () => {
+  const context = useContext(UserContext);
+
+  if (context === undefined) {
+    throw new Error('useUser must be used within a UserProvider');
+  }
+
+  return context;
+};
+
+export { UserProvider, useUser };
